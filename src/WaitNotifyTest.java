1c49000cd33700610c20450aabd0d7aef651ada6
g1uk <54111642+g1uk@users.noreply.github.com>
2019-10-13 21:45:52 +0300

    Initial commit

---
+++ src/WaitNotifyTest.java
@@ -0,0 +1,45 @@
+public class WaitNotifyTest {
+    private int myInc = 0;
+    private boolean finish = false;
+    private final int AMOUNT_OF_ITERATIONS = 1000000;
+
+    public static void main(String[] args) {
+        WaitNotifyTest waitNotifyTest = new WaitNotifyTest();
+        Runnable inc = waitNotifyTest::incremenator;
+        Thread increment = new Thread(inc);
+        increment.start();
+
+        Runnable incPrint = waitNotifyTest::printer;
+        Thread incrementPrint = new Thread(incPrint);
+        incrementPrint.start();
+    }
+    public synchronized void incremenator () {
+        while (myInc < AMOUNT_OF_ITERATIONS) {
+            myInc++;
+            notify();
+            try {
+                wait(100);
+            } catch (InterruptedException e) {
+
+            }
+
+        }
+
+        finish = true;
+        notifyAll();
+    }
+    public synchronized void printer () {
+        while (!finish) {
+            System.out.println(myInc);
+            notify();
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+
+
+    }
+}
+
